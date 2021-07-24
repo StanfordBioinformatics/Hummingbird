2b5bf9d992542ce6bafea7fbb3ec2913763e057a
Amir Alavi <amiralavi7@gmail.com>
2021-07-24 10:29:40 -0400

    Bump CloudHummingbird version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
         name='CloudHummingbird',
-        version='1.0.2',
+        version='1.1.0',
         description='A tool that recommends the best way to run your genomics pipelines on the cloud',
         long_description=long_description,
         long_description_content_type='text/markdown',
